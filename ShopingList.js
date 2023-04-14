0aa8017ea2de77a4accfd6799f7f4ddff57f809d
OMOLLO PINCHEZ <111060881+OmolloRovy@users.noreply.github.com>
2023-04-14 16:40:15 +0300

    CSS JavaScript and html
    The html has all the codes but it decided not to link to my JavaScript and CSS pages so I combined them into a single html "shopping List2"

---
+++ ShopingList.js
@@ -0,0 +1,18 @@
+const itemList = document.getElementById("item-list");
+const addButton = document.getElementById("add-button");
+const newItem = document.getElementById("new-item");
+
+addButton.addEventListener("click", function() {
+    if (newItem.value !== "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", function() {
+            li.remove();
+        });
+        li.innerText = newItem.value;
+        li.appendChild(deleteButton);
+        itemList.appendChild(li);
+        newItem.value = "";
+    }
+});
