41c90f235cc9535ceb6e20b007155cd2a666bdae
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2025-01-01 21:40:12 +0300

    Update ShopingList.js

--- ShopingList.js
+++ ShopingList.js
@@ -2,7 +2,7 @@
 const addButton = document.getElementById("add-button");
 const newItem = document.getElementById("new-item");
 
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function(newItem.value !== "") {
     if (newItem.value !== "") {
         const li = document.createElement("li");
         const deleteButton = document.createElement("button");
